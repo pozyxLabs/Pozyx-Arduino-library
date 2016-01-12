ad54cf22717a65df7b802418c89f748ae99ebc5a
Samuel <samuel@pozyx.io>
2016-01-12 12:13:17 +0100

    chat room changes to make it more tutorial friendly

--- examples/chat_room/chat_room.ino
+++ examples/chat_room/chat_room.ino
@@ -2,26 +2,27 @@
   The pozyx chat demo
   please check out https://www.pozyx.io/Documentation/Tutorials/getting_started
   
-  This demo requires at least two pozyx shields and two Arduino's. It demonstrates the wireless messaging capabilities of the
-  pozyx device.
+  This demo requires at least two pozyx shields and an equal number of Arduino's. 
+  It demonstrates the wireless messaging capabilities of the pozyx device.
   
   This demo creates a chat room. Text written in the Serial monitor will be broadcasted to all other pozyx devices
-  within range. They will see your message appear in their Serial monitor
+  within range. They will see your message appear in their Serial monitor.
 */
 
 #include "Pozyx.h"
 #include "Pozyx_definitions.h"
 #include <Wire.h>
 
-String inputString = "";             // a string to hold incoming data
-boolean stringComplete = false;      // whether the string is complete
-
 uint16_t source_id;                 // the network id of this device  
 uint16_t destination_id = 0;        // the destination network id. 0 means the message is broadcasted to every device in range
+String inputString = "";            // a string to hold incoming data
+boolean stringComplete = false;     // whether the string is complete
 
 void setup(){
   Serial.begin(115200);
-  if(! Pozyx.begin(false, 1, POZYX_INT_MASK_RX_DATA, 0)){
+
+  // initialize Pozyx
+  if(! Pozyx.begin(false, MODE_INTERRUPT, POZYX_INT_MASK_RX_DATA, 0)){
     Serial.println("ERROR: Unable to connect to POZYX shield");
     Serial.println("Reset required");
     abort();
@@ -29,68 +30,78 @@ void setup(){
   
   // read the network id of this device
   Pozyx.regRead(POZYX_NETWORK_ID, (uint8_t*)&source_id, 2);
+
+  // reserve 100 bytes for the inputString:
+  inputString.reserve(100);
   
   Serial.println("--- Pozyx Chat started ---");
 }
 
 void loop(){
-  inputString = "";
-  while (!stringComplete) {
-    
-    // See if we have some input from the serial monitor, if so, transmit it
-    serialEvent();
-    
-    // we wait up to 50ms to see if we have received an incoming message (if so we receive an RX_DATA interrupt)
-    if(Pozyx.waitForFlag(POZYX_INT_STATUS_RX_DATA,50))
-    {
-      // we have received a message!
-      
-      uint8_t length = 0;
-      uint16_t messenger = 0x00;      
-      delay(1);
-      // Let's read out some information about the message (i.e., how many bytes did we receive and who sent the message)
-      Pozyx.getLastDataLength(&length);      
-      Pozyx.getLastNetworkId(&messenger);
-      
-      char data[length];
 
-      // read the contents of the receive (RX) buffer, this is the message that was sent to this device
-      Pozyx.readRXBufferData((uint8_t *) data, length);
-      Serial.print("Ox");
-      Serial.print(messenger, HEX);
-      Serial.print(": ");
-      Serial.println(data);
-    }
-  }  
+  // check if we received a newline character and if so, broadcast the inputString.
+  if(stringComplete){
+    Serial.print("Ox");
+    Serial.print(source_id, HEX);
+    Serial.print(": ");
+    Serial.println(inputString);      
+    
+    int length = inputString.length();
+    uint8_t buffer[length];
+    inputString.getBytes(buffer, length);          
+   
+    // write the message to the transmit (TX) buffer
+    int status = Pozyx.writeTXBufferData(buffer, length);
+    // broadcast the contents of the TX buffer
+    status = Pozyx.sendTXBufferData(destination_id);
+    
+    inputString = "";    
+    stringComplete = false; 
+  }
+       
+  // we wait up to 50ms to see if we have received an incoming message (if so we receive an RX_DATA interrupt)
+  if(Pozyx.waitForFlag(POZYX_INT_STATUS_RX_DATA,50))
+  {
+    // we have received a message!
+    
+    uint8_t length = 0;
+    uint16_t messenger = 0x00;      
+    delay(1);
+    // Let's read out some information about the message (i.e., how many bytes did we receive and who sent the message)
+    Pozyx.getLastDataLength(&length);      
+    Pozyx.getLastNetworkId(&messenger);
+    
+    char data[length];
+
+    // read the contents of the receive (RX) buffer, this is the message that was sent to this device
+    Pozyx.readRXBufferData((uint8_t *) data, length);
+    Serial.print("Ox");
+    Serial.print(messenger, HEX);
+    Serial.print(": ");
+    Serial.println(data);
+  }
+ 
 }
 
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ For more info check http://www.arduino.cc/en/Tutorial/SerialEvent
+ */
 void serialEvent() {
-  
   while (Serial.available()) {
-    char inChar = (char)Serial.read();  
+    // get the new byte:
+    char inChar = (char)Serial.read();    
+    
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it.
+    // otherwise, add it to the inputString:
     if (inChar == '\n') {
       stringComplete = true;
-      
-      Serial.print("Ox");
-      Serial.print(source_id, HEX);
-      Serial.print(": ");
-      Serial.println(inputString);      
-      
-      int length = inputString.length();
-      uint8_t buffer[length];
-      inputString.getBytes(buffer, length);          
-     
-      // write the message to the transmit (TX) buffer
-      int status = Pozyx.writeTXBufferData(buffer, length);
-      // broadcast the contents of the TX buffer
-      status = Pozyx.sendTXBufferData(destination_id);
-      
-      inputString = "";
-      stringComplete = false;
-    }
-    else{
+    }else{
       inputString += inChar;
-      Serial.print(inChar);
     }
   }
 }
