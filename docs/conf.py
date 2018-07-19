1291a30043deaa701dd2f5b19f641e8025ca8912
Laurent Van Acker <laurent@pozyx.io>
2018-07-19 15:18:19 +0200

    Updated docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Samuel Van de Velde, Vadim Vermeiren, Laurent Van Acker'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '1.2.1'
+release = '1.2.2'
 
 
 # -- General configuration ---------------------------------------------------
@@ -74,7 +74,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
